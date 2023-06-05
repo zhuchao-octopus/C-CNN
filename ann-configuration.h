27f475c121a3eab74020b7ef8a1dbe3c8b2728ec
m <m@zhuchao.top>
2023-06-05 17:20:52 +0800

    add config

---
+++ ann-configuration.h
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ *  ann-configuration.h
+ *  Home Page :http://www.1234998.top
+ *  Created on: June 05, 2023
+ *  Author: M
+ */
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _INC_ANN_CONFIGURATION_H_
+#define _INC_ANN_CONFIGURATION_H_
+
+
+
+TPNeuralNet NeuralNetInit_C_CNN_16(char* NetName);
+TPNeuralNet NeuralNetInit_C_CNN_9(char* NetName);
+
+#endif
+
